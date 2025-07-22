da5915e8ef76a9096ae33c4145b4a64509047d51
uddhav-brai <uddhavbhattarai007@gmail.com>
2025-07-22 14:30:04 +0545

    useState parctise

---
+++ state/App.jsx
@@ -0,0 +1,25 @@
+import React from "react"
+import padsData from "./pads"
+import Pad from "./Pad"
+
+export default function App() {
+    const [pads, setPads] = React.useState(padsData)
+    
+    function toggle(id) {
+        setPads(prevPads => prevPads.map(item => {
+            return item.id === id ? {...item, on: !item.on} : item
+        }))
+    }
+    
+    const buttonElements = pads.map(pad => (
+        <Pad toggle={toggle} id={pad.id} key={pad.id} color={pad.color} on={pad.on}/>
+    ))
+    
+    return (
+        <main>
+            <div className="pad-container">
+                {buttonElements}
+            </div>
+        </main>
+    )
+}
---
+++ state/Pad.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Pad(props) {
+    return (
+        <button 
+            style={{backgroundColor: props.color}}
+            className={props.on ? "on" : undefined}
+            onClick={() => props.toggle(props.id)}
+        ></button>
+    )
+}
